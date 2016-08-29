9e0747676c0a5c9579f625c97e0bbcf81602a733
Daniel Maness <daniel.maness@gmail.com>
2016-08-29 16:04:09 -0700

    Initial Commit

---
+++ LayerKit-Diagnostics/LayerKit-Diagnostics.h
@@ -0,0 +1,19 @@
+//
+//  LayerKit-Diagnostics.h
+//  LayerKit-Diagnostics
+//
+//  Created by Daniel Maness on 8/29/16.
+//  Copyright © 2016 Layer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LayerKit-Diagnostics.
+FOUNDATION_EXPORT double LayerKit_DiagnosticsVersionNumber;
+
+//! Project version string for LayerKit-Diagnostics.
+FOUNDATION_EXPORT const unsigned char LayerKit_DiagnosticsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LayerKit_Diagnostics/PublicHeader.h>
+
+
